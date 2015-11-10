8c8543d6831f3d9cf1e45b79e9e92accaed079c7
yrsegal <yrsegal@gmail.com>
2015-11-10 00:03:57 -0500

    remove -

--- src/main/java/ninja/shadowfox/shadowfox_botany/lib/Constants.kt
+++ src/main/java/ninja/shadowfox/shadowfox_botany/lib/Constants.kt
@@ -3,7 +3,7 @@ package ninja.shadowfox.shadowfox_botany.lib
 
 object Constants {
     const val MODID = "shadowfox_botany"
-    const val MODNAME = "Botanical Add-ons"
+    const val MODNAME = "Botanical Addons"
     const val VERSION = "0.1.2"
     const val DEPENDENCIES = "required-after:Forge@[10.13.2.1291,);required-after:Botania@[r1.7-227,)"
 }
