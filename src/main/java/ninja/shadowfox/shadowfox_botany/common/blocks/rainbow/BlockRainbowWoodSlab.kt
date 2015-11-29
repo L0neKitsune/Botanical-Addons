d8d02eb19029bd0a92caea89e37c6b018e805f65
yrsegal <yrsegal@gmail.com>
2015-11-28 22:19:48 -0500

    fix slabs smelting at the same level as wood
    cheap shot, minecraft code, cheap shot

--- src/main/java/ninja/shadowfox/shadowfox_botany/common/blocks/rainbow/BlockRainbowWoodSlab.kt
+++ src/main/java/ninja/shadowfox/shadowfox_botany/common/blocks/rainbow/BlockRainbowWoodSlab.kt
@@ -1,5 +1,8 @@
 package ninja.shadowfox.shadowfox_botany.common.blocks.rainbow
 
+import cpw.mods.fml.common.IFuelHandler
+import cpw.mods.fml.common.registry.GameRegistry
+import net.minecraft.item.Item
 import net.minecraft.block.Block
 import net.minecraft.block.BlockSlab
 import net.minecraft.entity.player.EntityPlayer
@@ -11,10 +14,11 @@ import ninja.shadowfox.shadowfox_botany.common.lexicon.LexiconRegistry
 import vazkii.botania.api.lexicon.LexiconEntry
 
 
-class BlockRainbowWoodSlab(full: Boolean, source: Block = ShadowFoxBlocks.rainbowPlanks): ShadowFoxSlabs(full, 0, source, source.unlocalizedName.replace("tile.".toRegex(), "") + "Slab" + (if (full) "Full" else "")){
+class BlockRainbowWoodSlab(full: Boolean, source: Block = ShadowFoxBlocks.rainbowPlanks): ShadowFoxSlabs(full, 0, source, source.unlocalizedName.replace("tile.".toRegex(), "") + "Slab" + (if (full) "Full" else "")), IFuelHandler {
     init {
         this.setHardness(1.5f)
         this.setResistance(10.0f)
+        GameRegistry.registerFuelHandler(this)
     }
 
     override fun getFullBlock(): BlockSlab {
@@ -27,5 +31,9 @@ class BlockRainbowWoodSlab(full: Boolean, source: Block = ShadowFoxBlocks.rainbo
 
     override fun getEntry(world: World?, x: Int, y: Int, z: Int, player: EntityPlayer?, lexicon: ItemStack?): LexiconEntry {
         return LexiconRegistry.irisSapling
+    }
+
+    override fun getBurnTime(fuel: ItemStack): Int {
+        return if (fuel.item == Item.getItemFromBlock(this)) 150 else 0
     }
 }
