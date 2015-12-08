0bd8ebbcf35b1babde09cbdbdb1f87614e875de8
yrsegal <yrsegal@gmail.com>
2015-12-07 21:36:02 -0500

    trying to get NEI to work, having trouble
    if you could take a look, @L0neKitsune?

---
+++ src/main/java/ninja/shadowfox/shadowfox_botany/client/core/nei/RecipeHandlerTreeCrafting.kt
@@ -0,0 +1,48 @@
+package ninja.shadowfox.shadowfox_botany.client.nei
+
+import net.minecraft.item.ItemStack
+import net.minecraft.util.StatCollector
+
+import vazkii.botania.api.recipe.RecipePetals
+import vazkii.botania.client.core.handler.HUDHandler
+import vazkii.botania.client.integration.nei.RecipeHandlerPetalApothecary
+import vazkii.botania.client.integration.nei.RecipeHandlerPetalApothecary.CachedPetalApothecaryRecipe
+import vazkii.botania.common.block.tile.mana.TilePool
+
+import ninja.shadowfox.shadowfox_botany.api.ShadowFoxAPI
+import ninja.shadowfox.shadowfox_botany.api.recipe.RecipeTreeCrafting
+import ninja.shadowfox.shadowfox_botany.common.blocks.ShadowFoxBlocks
+
+import codechicken.nei.PositionedStack
+
+class RecipeHandlerTreeCrafting: RecipeHandlerPetalApothecary() {
+
+    public class CachedTreeCrafterRecipe(recipe: RecipeTreeCrafting): CachedPetalApothecaryRecipe(recipe, false) {
+
+        var manaUsage: Int = 0
+
+        init{
+            if(recipe != null) {
+                manaUsage = recipe.getManaUsage()
+                inputs.add(PositionedStack(ItemStack(ShadowFoxBlocks.irisSapling), 73, 55))
+            }
+        }
+
+    }
+
+    override fun getRecipeName(): String = StatCollector.translateToLocal("shadowfox_botany.nei.treeCrafter")
+    override fun getRecipeID(): String = "shadowfox_botany.treeCrafter"
+
+    override fun drawBackground(recipe: Int) {
+        super.drawBackground(recipe)
+        HUDHandler.renderManaBar(32, 113, 0x0000FF, 0.75F, (arecipes.get(recipe) as CachedTreeCrafterRecipe).manaUsage, TilePool.MAX_MANA / 10)
+    }
+
+    override fun getRecipes(): MutableList<out RecipePetals> {
+        return ShadowFoxAPI.treeRecipes
+    }
+
+    override fun getCachedRecipe(recipe: RecipePetals): CachedPetalApothecaryRecipe {
+        return CachedTreeCrafterRecipe(recipe as RecipeTreeCrafting)
+    }
+}
