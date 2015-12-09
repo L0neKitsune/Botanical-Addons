f1500c8c625e041bee022021b9e66c1d02b6a099
yrsegal <yrsegal@gmail.com>
2015-12-08 22:05:31 -0500

    you're only allowed to use utils from within the API for this

--- src/main/java/ninja/shadowfox/shadowfox_botany/api/recipe/RecipeTreeCrafting.kt
+++ src/main/java/ninja/shadowfox/shadowfox_botany/api/recipe/RecipeTreeCrafting.kt
@@ -3,12 +3,30 @@ package ninja.shadowfox.shadowfox_botany.api.recipe
 import cpw.mods.fml.common.FMLLog
 import net.minecraft.block.Block
 import net.minecraft.item.ItemStack
-import ninja.shadowfox.shadowfox_botany.common.utils.itemEquals
+import net.minecraftforge.oredict.OreDictionary
 import org.apache.logging.log4j.Level
 import java.util.*
 
 import vazkii.botania.api.recipe.RecipePetals
 
+internal fun ItemStack.itemEquals(rItem: Any): Boolean {
+    if (rItem is String) {
+
+        for (stack in OreDictionary.getOres(rItem)) {
+            val cstack = stack.copy()
+
+            if (cstack.itemDamage == 32767) cstack.itemDamage = this.itemDamage
+            if (this.isItemEqual(cstack)) return true
+        }
+
+    } else if (rItem is ItemStack && simpleAreStacksEqual(rItem, this)) return true
+    else return false
+    return false
+}
+
+internal fun simpleAreStacksEqual(stack: ItemStack, stack2: ItemStack): Boolean {
+    return stack.item === stack2.item && stack.itemDamage == stack2.itemDamage
+}
 
 public class RecipeTreeCrafting(val mana: Int, val outputBlock: Block, val meta: Int, vararg inputs: Any): RecipePetals(ItemStack(outputBlock, 1, meta), *inputs) {
 
