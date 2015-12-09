2af1ad1a578091dae94975d06b2265cb1cd816aa
yrsegal <yrsegal@gmail.com>
2015-12-08 20:21:40 -0500

    comment out testingRod
    ._.

--- src/main/java/ninja/shadowfox/shadowfox_botany/common/item/ShadowFoxItems.kt
+++ src/main/java/ninja/shadowfox/shadowfox_botany/common/item/ShadowFoxItems.kt
@@ -21,7 +21,7 @@ object ShadowFoxItems {
     val attributionBauble: Item
     val wiltedLotus: Item
 
-    val testingRod: Item
+    // val testingRod: Item
     //    val splashPotion: Item
 
     init {
