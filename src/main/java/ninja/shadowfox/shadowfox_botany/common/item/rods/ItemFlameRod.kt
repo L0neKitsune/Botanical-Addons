87c5b637f6aa718fbe3018b889d8efc8a908ac8f
yrsegal <yrsegal@gmail.com>
2016-01-28 15:37:21 -0500

    remove avatar functionality

--- src/main/java/ninja/shadowfox/shadowfox_botany/common/item/rods/ItemFlameRod.kt
+++ src/main/java/ninja/shadowfox/shadowfox_botany/common/item/rods/ItemFlameRod.kt
@@ -5,17 +5,11 @@ import cpw.mods.fml.relauncher.FMLLaunchHandler
 import cpw.mods.fml.relauncher.Side
 import cpw.mods.fml.relauncher.SideOnly
 import net.minecraft.client.renderer.texture.IIconRegister
-import net.minecraft.command.IEntitySelector
-import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityLivingBase
-import net.minecraft.entity.IProjectile
 import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.entity.player.EntityPlayerMP
 import net.minecraft.item.EnumAction
 import net.minecraft.item.ItemStack
-import net.minecraft.tileentity.TileEntity
 import net.minecraft.util.AxisAlignedBB
-import net.minecraft.util.DamageSource
 import net.minecraft.util.EntityDamageSource
 import net.minecraft.util.ResourceLocation
 import net.minecraft.world.World
@@ -25,23 +19,18 @@ import ninja.shadowfox.shadowfox_botany.api.item.ColorOverrideHelper
 import ninja.shadowfox.shadowfox_botany.common.item.ItemMod
 import ninja.shadowfox.shadowfox_botany.common.item.baubles.ItemPriestEmblem
 import ninja.shadowfox.shadowfox_botany.common.utils.helper.InterpolatedIconHelper
-import vazkii.botania.api.internal.IManaBurst
-import vazkii.botania.api.item.IAvatarTile
-import vazkii.botania.api.item.IAvatarWieldable
 import vazkii.botania.api.item.IManaProficiencyArmor
 import vazkii.botania.api.mana.IManaUsingItem
 import vazkii.botania.api.mana.ManaItemHandler
 import vazkii.botania.common.Botania
 import vazkii.botania.common.core.helper.Vector3
-import vazkii.botania.common.entity.EntityDoppleganger
 import java.awt.Color
 
 /**
  * @author WireSegal
  * Created at 9:32 PM on 1/27/16.
  */
-public open class ItemFlameRod(name: String = "flameRod") : ItemMod(name), IManaUsingItem, IAvatarWieldable {
-    private val avatarOverlay = ResourceLocation("shadowfox_botany:textures/model/avatarFlame.png")
+public open class ItemFlameRod(name: String = "flameRod") : ItemMod(name), IManaUsingItem {
 
     init {
         setMaxStackSize(1)
@@ -96,9 +85,9 @@ public open class ItemFlameRod(name: String = "flameRod") : ItemMod(name), IMana
             var prowess = IManaProficiencyArmor.Helper.hasProficiency(player)
 
             val color = Color(ColorOverrideHelper.getColor(player, 0xF94407))
-            val r = color.red/255f
-            val g = color.green/255f
-            val b = color.blue/255f
+            val r = color.red / 255f
+            val g = color.green / 255f
+            val b = color.blue / 255f
 
             Botania.proxy.sparkleFX(world, dx, dy, dz, r, g, b, 1f, 5)
 
@@ -121,8 +110,6 @@ public open class ItemFlameRod(name: String = "flameRod") : ItemMod(name), IMana
     val PROWESS_COST = -2
     val PRIEST_COST = 3
 
-    val AVATAR_COST = 4
-
     val DAMAGE = 2
     val PROWESS_DAMAGE = 1
     val PRIEST_DAMAGE = 7
@@ -139,30 +126,5 @@ public open class ItemFlameRod(name: String = "flameRod") : ItemMod(name), IMana
         if (prowess) d += PROWESS_DAMAGE
         if (priest) d += PRIEST_DAMAGE
         return d
-    }
-
-    override fun onAvatarUpdate(tile: IAvatarTile, stack: ItemStack) {
-//        val te = tile as TileEntity
-//        var world = te.worldObj
-//        var x = te.xCoord.toDouble()
-//        var y = te.yCoord.toDouble()
-//        var z = te.zCoord.toDouble()
-//
-//        if (tile.currentMana >= AVATAR_COST) {
-//            if (tile.elapsedFunctionalTicks % 5 == 0) particleRing(world, x, y, z, RANGE, 0f, 0f, 1f)
-//
-//            val entities = world.selectEntitiesWithinAABB(EntityLivingBase::class.java,
-//                    AxisAlignedBB.getBoundingBox(x - RANGE, y - RANGE, z - RANGE,
-//                            x + RANGE, y + RANGE, z + RANGE), AVATAR_SELECTOR)
-//
-//            if (pushEntities(x, y, z, RANGE, VELOCITY, entities)) {
-//                if (tile.elapsedFunctionalTicks % 3 == 0) world.playSoundEffect(x.toDouble(), y.toDouble(), z.toDouble(), "shadowfox_botany:wind", 0.4F, 1F)
-//                tile.recieveMana(-AVATAR_COST)
-//            }
-//        }
-    }
-
-    override fun getOverlayResource(tile: IAvatarTile, stack: ItemStack): ResourceLocation {
-        return avatarOverlay
     }
 }
